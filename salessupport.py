8dd9939c3eb7b14db5eab83962122da2f7ca45a5
oitnelav <valentiocoding@gmail.com>
2025-01-11 09:41:43 +0700

    Update salessupport.py

--- salessupport.py
+++ salessupport.py
@@ -61,7 +61,8 @@ file_catalogue = st.session_state.catalogue
 
 file_catalogue = st.file_uploader("Upload File Catalogue Update", type=["xlsx", "xls", "csv"])
 
-st.dataframe(file_catalogue)
+if file_catalogue:
+    st.dataframe(file_catalogue)
 
 st.dataframe(database[database['ItemCode'] == 'BDO-1055M'])
 
