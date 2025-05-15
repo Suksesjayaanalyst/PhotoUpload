8abccb36527333f72090235d34f71cffaeee511c
Suksesjayaanalyst <fotoproducttpf@gmail.com>
2025-05-15 14:07:33 +0700

    Update byfile.py

--- byfile.py
+++ byfile.py
@@ -190,7 +190,11 @@ if photo:
         def add_text(template, draw, row, font, selectprice):
             item_code = row['ItemCode']
             item_name = row['ItemName']
-            harga_jual = f"Rp. {row[selectprice]:,} / {row['Uom']}"
+            try:
+                price = float(row[selectprice])
+                harga_jual = f"Rp. {price:,.0f} / {row['Uom']}"
+            except ValueError:
+                harga_jual = f"Rp. {row[selectprice]} / {row['Uom']}"
             ctn = f"Isi Karton: {int(row['IsiCtn'])} {row['Uom']}" if pd.notna(row['IsiCtn']) else "N/A"
 
             # Wrap each line of text
