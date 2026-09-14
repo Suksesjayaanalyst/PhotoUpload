d3a0530055fd5e1f4af14602fa1d68319f75b5e9
Theo <theobunaaprilio95@yahoo.com>
2026-09-14 15:37:12 +0700

    Create landscape3.py

---
+++ landscape3.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+
+#SetupPage
+bylist = st.Page(
+     page="bylist.py",
+     title="Versi 1",
+     icon="📃",
+     default=True,
+)
+
+FotoKT = st.Page(
+     page="bylist2_fotokt.py",
+     title="Versi 1.1 (Foto KT)",
+     icon="📃",
+)
+
+Reqabdul = st.Page(
+     page="bylist4.py",
+     title="Versi 1.2 (3 Harga)",
+     icon="📃",
+)
+
+byfile = st.Page(
+     page="byfile.py",
+     title="Versi 2",
+     icon="📁",
+)
+
+byfile2 = st.Page(
+     page="byfile2_fotocustom.py",
+     title="Versi 2.1",
+     icon="📁",
+)
+
+discount = st.Page(
+     page="discount.py",
+     title="Discount",
+     icon="💸",
+)
+
+TasKarung = st.Page(
+     page="bylist3.py",
+     title="Versi Tas&Karung",
+     icon="📁",
+)
+
+# salessuport = st.Page(
+#      page="salessupport.py",
+#      title="Sales Support",
+#      icon="📈",
+# )
+
+pg = st.navigation({
+     "Choose": [bylist, FotoKT, Reqabdul, byfile, byfile2, discount, TasKarung],
+})
+
+pg.run()
